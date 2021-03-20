0e3dd05f062cfdbb434e63387f26d410e88185ea
cssanyi <sandor.cserhati@gmail.com>
2021-03-20 18:40:47 +0100

    basic argument validation

--- src/main/java/folderwatch/FolderWatch.java
+++ src/main/java/folderwatch/FolderWatch.java
@@ -1,31 +1,78 @@
 package folderwatch;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Spec;
 
 @Command(name = "FolderWatch", version = "FolderWatch 1.0.0", mixinStandardHelpOptions = true)
 public class FolderWatch implements Runnable {
 
+	private String incomingFolderPath;
+	int wordPairOccurenceThreshold = 3;
+	int threadCount = 1;
+	int exitDelay = 60;
+
+	@Spec
+	CommandSpec spec;
+
 	@Option(names = { "-i" }, required = true, description = "Absolute or relative folder path for incoming files")
-	String incomingFolderPath = "";
+	public void setIncomingFolderPath(String value) {
+		if (!isPathValid(value)) {
+			throw new ParameterException(spec.commandLine(),
+					String.format("Invalid value '%s' for option '-i'.", value));
+		}
+		incomingFolderPath = value;
+	}
 
 	@Option(names = { "-n" }, description = "Word pairs are included in the result above this occurence threshold (3)")
-	int wordPairOccurenceThreshold = 3;
+	public void setWordPairOccurenceThreshold(int value) {
+		if (value < 0) {
+			throw new ParameterException(spec.commandLine(),
+					String.format("Invalid value '%d' for option '-n': " + "value should not be negative.", value));
+		}
+		wordPairOccurenceThreshold = value;
+	}
 
 	@Option(names = { "-t" }, description = "Thread count (1)")
-	int threadCount = 1;
+	public void setThreadCount(int value) {
+		if (value < 1 || value > 99) {
+			throw new ParameterException(spec.commandLine(), String.format(
+					"Invalid value '%d' for option '-t': " + "value should be between 1 and 99 inclusive.", value));
+		}
+		threadCount = value;
+	}
 
 	@Option(names = { "-d" }, description = "Delay in seconds to wait for files idle before exit (60)")
-	int exitDelay = 60;
+	public void setExitDelay(int value) {
+		if (value < 0) {
+			throw new ParameterException(spec.commandLine(), String.format(
+					String.format("Invalid value '%d' for option '-d': " + "value should not be negative.", value)));
+		}
+		exitDelay = value;
+	}
+
+	private static boolean isPathValid(String path) {
+		try {
+			Paths.get(path);
+		} catch (InvalidPathException ex) {
+			return false;
+		}
+		return true;
+	}
 
 	@Override
 	public void run() {
-		System.out.println("hello: " + incomingFolderPath);
+		System.out.println(incomingFolderPath);
 	}
 
 	public static void main(String[] args) {
-			new CommandLine(new FolderWatch()).execute(args);
+		new CommandLine(new FolderWatch()).execute(args);
 	}
 
 }
