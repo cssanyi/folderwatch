07ac6e178d3a75b358f23a7897807035077fe07b
cssanyi <sandor.cserhati@gmail.com>
2021-03-20 15:45:43 +0100

    mvn wrapper, pom settings (java version and manifest)

---
+++ src/main/java/folderwatch/Main.java
@@ -0,0 +1,12 @@
+package folderwatch;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+		new Scanner(System.in).nextLine();
+	}
+
+}
